d9f0fc83fa4c3b6d4e1833fb7317d185d78e54c3
AndrewThien <anretrithien@gmail.com>
2025-03-31 17:12:18 +0100

    test scheduler

--- airflow/entrypoint.sh
+++ airflow/entrypoint.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -e
 
-# Initialize the database
-airflow db init
 
-# Create admin user
-airflow users create \
-  --username admin \
-  --password admin \
-  --firstname Air \
-  --lastname Flow \
-  --role Admin \
-  --email admin@example.com
-
-# Start the webserver
-exec airflow webserver
+# Start the scheduler
+exec airflow scheduler
